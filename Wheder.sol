77d92b6474281bae43e6548ddbdb55f396c475cb
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-11-10 10:16:06 +0100

    abi e compiler

---
+++ Wheder.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+contract Wheder {
+    string public Wheder_city;
+
+    function upgrade_wheder(string memory _wheder) public {
+        Wheder_city = _wheder;
+    }
+
+    function get_wheder() public view returns (string memory) {
+        return Wheder_city;
+    }
+}
---
+++ compile.js
@@ -0,0 +1,34 @@
+const solc = require("solc");
+const path = require("path");
+const fs = require("fs");
+
+const contractName = "Wheder";
+const file_name = `${contractName}.sol`;
+
+const file_path = fs.readFileSync(__dirname + "/" + file_name, "utf8");
+
+var input = {
+  language: "Solidity",
+  sources: {
+    [file_name]: {
+      content: file_path,
+    },
+  },
+  settings: {
+    outputSelection: {
+      "*": {
+        "*": ["*"],
+      },
+    },
+  },
+};
+
+const compile_code = JSON.parse(solc.compile(JSON.stringify(input)));
+
+const bytecode = compile_code.contracts[file_name][contractName].evm.bytecode.object;
+
+fs.writeFileSync(__dirname + "/" + "ContractBytecode.bin", bytecode);
+
+// dentro all'abi ci sono le funzioni del contratto
+const abi = compile_code.contracts[file_name][contractName].abi;
+fs.writeFileSync(__dirname + "/" + 'ContractAbi.json', JSON.stringify(abi, null, "\t"));
