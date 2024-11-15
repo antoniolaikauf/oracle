478d270c00de879e03f1196e28999a105adadadb
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-11-15 15:25:17 +0100

    deliver

--- ContractCTC.sol
+++ ContractCTC.sol
@@ -8,8 +8,9 @@ contract Tct {
     uint256 public constant Gcncl = 10000 wei; // gas cancel
     uint256 public constant Go = 10000 wei; //  Gas needed for Deliver on a canceled request
     uint256 private Gclbk; // gaslimit per callback
-    address private callback;
-    uint256 private f;
+    address private callback; // address a cui inviare i dati
+    uint256 private f; // fee quanto è disposto a pagare l'utente
+    uint256[] public deposit_f; // deposito
     address private immutable i_address_Owner;
 
     struct params {
@@ -26,7 +27,7 @@ contract Tct {
         address cu; // address contratto
     }
 
-    mapping(address => data) message1;
+    mapping(uint256 => data) message1;
     mapping(uint256 => bytes32) params_store;
     mapping(uint256 => bool) isCanceled;
     mapping(uint256 => bool) isDelivered;
@@ -44,18 +45,23 @@ contract Tct {
         uint256 _gReq // viene messa ma non viene usata nel request
     ) public {
         assert(Gmin <= _f && _f < Gmax);
+
         uint256 Id = Counter;
         Counter += 1;
         f = _f;
+        deposit_f.push(f);
         callback = _callback;
+
         params memory params_data = params(_paramUrl, _paramSpec, _T);
-
         data memory store_data = data(params_data, Id, callback, f, msg.sender);
+        message1[Id] = store_data;
 
-        bytes32 params_hash = keccak256(abi.encode(_paramUrl, _paramSpec, _T));
-
+        /*
+         scelto trasfrmarlo in hash essendo che nel deliver
+         non si possono fare il ocnfronto tra due struct
+        */
+        bytes32 params_hash = keccak256(abi.encode(_paramUrl, _paramSpec, _T)); 
         params_store[Id] = params_hash;
-        message1[msg.sender] = store_data;
     }
 
     function deliver(
@@ -75,9 +81,7 @@ contract Tct {
             return;
         }
 
-        bytes32 params_hash = params_store[_id]; // da bloccare se non trovati
-
-        params memory params1 = params(_paramUrl, _paramSpec, _T);
+        bytes32 params_hash = params_store[_id]; // abort if not found crearlo 
 
         bytes32 params_hash1 = keccak256(abi.encode(_paramUrl, _paramSpec, _T));
 
@@ -86,7 +90,12 @@ contract Tct {
         payable(msg.sender).transfer(f);
         Gclbk = f - Gmin;
 
-        callback.call{gas:Gclbk}(abi.);// invio dati a callback
+        bytes4 selector = bytes4(keccak256("funzione a chi inviare"));
+        (bool success, bytes memory data) = callback.call{gas: Gclbk}(
+            abi.encodeWithSelector(selector, _data)
+        );
+
+        require(success, "fallito");
     }
 
     function cancel() public {}
